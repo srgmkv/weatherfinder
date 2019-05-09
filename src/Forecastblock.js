570a6d47e4e33f0f64b5362b53c7cb58e62f1d12
Serge W <ssa990@ya.ru>
2019-05-09 14:43:15 +0700

    *

--- src/Forecastblock.js
+++ src/Forecastblock.js
@@ -9,16 +9,16 @@ export default class ForeCastBlock extends React.Component {
   render() {
     //
     const list = this.props.requestForecast.list.filter((item, ind) => {
-      if (ind % 5 === 0)  return item });
+      if (ind % 5 === 0) return item
+    });
     console.log('Forecast', list)
     const labels = list.map(item => {
       const data = new Date(item.dt_txt)
       const date = data.getDate();
-      const month = data.getMonth();
-      const mlist = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+      const month = data.getMonth();const mlist = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
       const hours = data.getHours();
-      return `${date} ${mlist[month]} ${hours}h`
-      })
+      return `${date}/${month+1} ${hours}h`
+    })
     console.log('labels', labels)
 
     const data = {
@@ -28,13 +28,17 @@ export default class ForeCastBlock extends React.Component {
           label: 'Temp',
           fill: false,
           lineTension: 0.1,
-          borderColor: 'rgba(75,192,192,1)',
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: list.map(item => Math.round(item.main.temp - 273))
+          borderColor: 'rgba(223, 117, 41 ,1)',
+          pointRadius: 4,
+          pointHitRadius: 5,
+          pointStyle: 'rectRounded',
+          data: list.map(item => Math.round(item.main.temp - 273)),
+
         }
       ]
     };
+
+
 
 
     return (
