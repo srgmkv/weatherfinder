62cf4a902d49af385dfba8069595de5d8ae63c5c
Serge W <ssa990@ya.ru>
2019-05-10 21:50:14 +0700

    build add

---
+++ build/precache-manifest.f054056c9dbc32d6dece803d13212026.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998f3e7bd42a5014754a6bc120aa31b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd8b6da762b4e8337af1",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c202828fe80a6e91a3c0",
+    "url": "/static/css/main.0a4f481d.chunk.css"
+  },
+  {
+    "revision": "dd8b6da762b4e8337af1",
+    "url": "/static/js/2.5bf1cff1.chunk.js"
+  },
+  {
+    "revision": "c202828fe80a6e91a3c0",
+    "url": "/static/js/main.f73dd0c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "27acf8313b7676fefda6898bd0c46c27",
+    "url": "/static/media/Open-weather-main-page.27acf831.png"
+  }
+]);
