78d87cebf42bbaa90437243e7dcfa0a078436568
srgmkv <ms5533@yandex.ru>
2019-06-01 01:22:54 +0700

    Updates

---
+++ precache-manifest.e1224d03e9031ec332872a03eea6bd60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a81c4b146e5b525d924056f8451b1660",
+    "url": "/weatherfinder/index.html"
+  },
+  {
+    "revision": "7a7f740a0dfa766a4768",
+    "url": "/weatherfinder/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "decdd3245ebcb586b265",
+    "url": "/weatherfinder/static/css/main.4797a188.chunk.css"
+  },
+  {
+    "revision": "7a7f740a0dfa766a4768",
+    "url": "/weatherfinder/static/js/2.497d1290.chunk.js"
+  },
+  {
+    "revision": "decdd3245ebcb586b265",
+    "url": "/weatherfinder/static/js/main.90cb2f9c.chunk.js"
+  },
+  {
+    "revision": "22db855434434f9b4fbe",
+    "url": "/weatherfinder/static/js/runtime~main.31418dd9.js"
+  },
+  {
+    "revision": "27acf8313b7676fefda6898bd0c46c27",
+    "url": "/weatherfinder/static/media/Open-weather-main-page.27acf831.png"
+  }
+]);
